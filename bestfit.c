544e5c58fb62d5bb909264f0bc67448f18cc9a06
Subhani-syed <subhani6352@gmail.com>
2022-03-02 22:37:25 +0530

    Best Fit

--- bestfit.c
+++ bestfit.c
@@ -1,46 +1,53 @@
-#include <stdio.h>
-
-void main()
-{
-    int fragment[20], b[20], p[20], i, j, nb, np, temp, lowest = 9999;
-    static int barray[20], parray[20];
-    printf("\n\t\t\tMemory Management Scheme - Best Fit");
-    printf("\nEnter the number of blocks:");
-    scanf("%d", &nb);
-    printf("Enter the number of processes:");
-    scanf("%d", &np);
-    printf("\nEnter the size of the blocks:-\n");
-    for (i = 1; i <= nb; i++)
-    {
-        printf("Block no.%d:", i);
-        scanf("%d", &b[i]);
+#include<stdio.h>
+int main(){
+    int nob,nop;
+    printf("Enter the number of Blocks: ");
+    scanf("%d",&nob);
+    int block_size[nob];
+    for(int i=0;i<nob;i++){
+        printf("Enter Size of Block-%d: ",i+1);
+        scanf("%d",&block_size[i]);
     }
-    printf("\nEnter the size of the processes :-\n");
-    for (i = 1; i <= np; i++)
-    {
-        printf("Process no.%d:", i);
-        scanf("%d", &p[i]);
+    printf("Enter the number of Processes: ");
+    scanf("%d",&nop);
+    int process_size[nop];
+    for(int i=0;i<nop;i++){
+        printf("Enter Size of Process-%d: ",i+1);
+        scanf("%d",&process_size[i]);
     }
-    for (i = 1; i <= np; i++)
-    {
-        for (j = 1; j <= nb; j++)
-        {
-            if (barray[j] != 1)
-            {
-                temp = b[j] - p[i];
-                if (temp >= 0)
-                    if (lowest > temp)
-                    {
-                        parray[i] = j;
-                        lowest = temp;
-                    }
+    int available[nob];
+    for(int i=0;i<nob;i++){
+        available[i] = 1;
+    }
+    int frag[nob];
+    int block_alloc[nop];
+    printf("Process\t\tBlock Allocated\tProcessSize\tFrag\n");
+    
+    for(int i=0;i<nop;i++){
+        int flag = 0;
+        int block_allocated = -1;
+        int fragment = 9999999;
+        for(int j=0;j<nob;j++){
+            if(available[j]){
+                if(block_size[j] - process_size[i]<fragment){
+                    block_allocated = j;
+                    fragment = block_size[j] - process_size[i];
+                    flag =1;
+                }
             }
         }
-        fragment[i] = lowest;
-        barray[parray[i]] = 1;
-        lowest = 10000;
+        frag[i] = block_size[block_allocated] - process_size[i];
+        available[block_allocated] = 0;
+        block_alloc[i] = block_allocated;
+        printf("%d\t\t",i+1);
+        if(flag==0){
+            printf("Not Allocated\t\t");
+            printf("%d\n",process_size[i]);
+        }else{
+            printf("%d\t\t",block_alloc[i]);
+            printf("%d\t\t",process_size[i]);
+            printf("%d\n",frag[i]);
+        }
     }
-    printf("\nProcess_no\tProcess_size\tBlock_no\tBlock_size\tFragment");
-    for (i = 1; i <= np && parray[i] != 0; i++)
-        printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", i, p[i], parray[i], b[parray[i]], fragment[i]);
+    return 0;
 }
