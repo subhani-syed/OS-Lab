ac6548d47e0d9ae81f771f00989d5ca9f4c09a39
Subhani-syed <subhani6352@gmail.com>
2022-03-03 07:50:46 +0530

    MVT

--- mvt.c
+++ mvt.c
@@ -1,29 +1,24 @@
-#include <stdio.h>
-void main()
-{
-    int m = 0, m1 = 0, m2 = 0, p, count = 0, i;
-    printf("enter the memory capacity:");
-    scanf("%d", &m);
-    printf("enter the no of processes:");
-    scanf("%d", &p);
-    for (i = 0; i < p; i++)
-    {
-        printf("\nenter memory req for process%d: ", i + 1);
-        scanf("%d", &m1);
-        count = count + m1;
-        if (m1 <= m)
-        {
-            if (count == m)
-                printf("there is no further memory remaining:");
-            printf("the memory allocated for process%d is: %d ", i + 1, m);
-            m2 = m - m1;
-            printf("\nremaining memory is: %d", m2);
-            m = m2;
-        }
-        else
-        {
-            printf("memory is not allocated for process%d", i + 1);
-        }
-        printf("\nexternal fragmentation for this process is:%d", m2);
-    }
-}
+  #include<stdio.h>
+ int main(){
+     int n;
+     printf("Enter the number of process: ");
+     scanf("%d",&n);
+     int memory;
+     printf("Enter the size of memory: ");
+     scanf("%d",&memory);
+     for(int i=0;i<n;i++){
+         printf("Enter the size of process %d : ",i+1);
+         int process_size;
+         scanf("%d",&process_size);
+         if(process_size<=memory){
+             printf("Memory Allocated is %d\n",memory);
+             memory = memory - process_size;
+             printf("Remaining Memory is %d \n",memory);
+             printf("External Fragmentation is %d \n",memory);
+         }else{
+             printf("Memory Not Allocated for process %d\n",i+1);
+             printf("External Fragmentation is %d \n",memory);
+         }
+     }
+     return 0;
+ }
