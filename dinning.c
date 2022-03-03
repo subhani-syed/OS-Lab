6cdc40ebefa331f3abad279c4924aff485caf53a
Subhani-syed <subhani6352@gmail.com>
2022-03-03 07:16:54 +0530

    Dinning Philosopher's Problem

--- dinning.c
+++ dinning.c
@@ -7,26 +7,6 @@
 sem_t room;
 sem_t chopstick[5];
 
-void * philosopher(void *);
-void eat(int);
-int main()
-{
-	int i,a[5];
-	pthread_t tid[5];
-	
-	sem_init(&room,0,4);
-	
-	for(i=0;i<5;i++)
-		sem_init(&chopstick[i],0,1);
-		
-	for(i=0;i<5;i++){
-		a[i]=i;
-		pthread_create(&tid[i],NULL,philosopher,(void *)&a[i]);
-	}
-	for(i=0;i<5;i++)
-		pthread_join(tid[i],NULL);
-}
-
 void * philosopher(void * num)
 {
 	int phil=*(int *)num;
@@ -49,3 +29,21 @@ void eat(int phil)
 {
 	printf("\nPhilosopher %d is eating",phil);
 }
+
+int main()
+{
+	int i,a[5];
+	pthread_t tid[5];
+	sem_init(&room,0,4);
+	for(i=0;i<5;i++)
+		sem_init(&chopstick[i],0,1);
+	for(i=0;i<5;i++){
+		a[i]=i;
+		pthread_create(&tid[i],NULL,philosopher,(void *)&a[i]);
+	}
+	for(i=0;i<5;i++)
+		pthread_join(tid[i],NULL);
+
+	return 0;
+}
+
